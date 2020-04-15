30ab9a4451dd8c09254992b74a37b6a79793b179
AdrianCiuciui <adrian.ciuciui.cornel@gmail.com>
2020-04-15 09:28:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scan = new Scanner(System.in);
+    private static MobilePhone myAgenda = new MobilePhone();
+
+
+    public static void main (String[] args) {
+        myAgenda.printMenu();
+        while (true) {
+            int choice = 0;
+            choice = scan.nextInt(); scan.nextLine();
+            switch (choice) {
+                case 1:
+                    myAgenda.printContacts(); break;
+                case 2:
+                    addContact(); break;
+                case 3:
+                    deleteContact(); break;
+                case 4:
+                    modifyContact(); break;
+                case 5:
+                    findContact(); break;
+                default: break;
+            }
+            if (choice == 0) break;
+        }
+    }
+
+    public static void addContact () {
+        System.out.println("Insert name");
+        String name = scan.nextLine();
+        System.out.println("Insert phone number");
+        String number = scan.nextLine();
+        Contact contact = new Contact(name, number);
+        myAgenda.addContact(contact);
+    }
+
+    public static void deleteContact () {
+        System.out.println("Input name of the contact you want deleted");
+        String input = scan.nextLine();
+        myAgenda.deleteContact(input);
+    }
+
+    public static void modifyContact () {
+        System.out.println("Input the name you want to modify - original");
+        String name = scan.nextLine();
+        System.out.println("Input the name you want to change to ");
+    }
+
+    public static void findContact () {
+        System.out.println("Input name you want to find");
+        String name = scan.nextLine();
+        if (myAgenda.findContact2(name)) System.out.println(name + " was found in the phone's agenda");
+        else System.out.println(name + " was not found in the phone's agenda");
+    }
+}
---
+++ src/MobilePhone.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+public class MobilePhone {
+    
+    private ArrayList<Contact> list = new ArrayList<Contact>();
+
+    //public methods
+
+    public ArrayList <Contact> getList () {
+        return list;
+    }
+
+    public void addContact (Contact contact) {
+        this.list.add(contact);
+    }
+
+    public void deleteContact (String contact) {
+        int position = findContact(contact);
+        if (position >= 0) deleteContact(position);
+        System.out.println("Contact from position " + position + "deleted");
+    }
+
+//    public void modifyContact (Contact oldC, Contact newC) {
+//        int position = findContact(oldC);
+//        if (position >= 0) {
+//            System.out.println(oldC.getName() + " with number " + oldC.getNumber() + " will be overwritten by:");
+//            modifyContact(position, newC);
+//        }
+//        else System.out.println("No changes have been made to the list");
+//    }
+
+    public void printContacts () {
+        System.out.println("Your " + list.size() + " contact(s) are:");
+        for (int i = 0; i< list.size(); i++) System.out.println((i+1) + ". " + list.get(i));
+    }
+
+    public void printMenu () {
+        System.out.println("This is the Menu - choose what to do by inputting a number.\n" +
+                "0. quit\n" +
+                "1. display contacts\n" +
+                "2. add contact\n" +
+                "3. delete contact\n" +
+                "4. edit contact.\n" +
+                "5. search contact");
+    }
+
+    public boolean findContact2(String name) {
+        return findContact(name) >= 0;
+    }
+
+    // private methods
+
+    private int findContact(String name) {
+        return list.indexOf(name);
+    }
+
+    private void deleteContact (int position) {
+        list.remove(position);
+    }
+
+    private void modifyContact (int position, Contact newC) {
+        list.set(position, newC);
+        System.out.println(newC.getName() + " with number " + newC.getNumber());
+    }
+
+
+
+
+}
